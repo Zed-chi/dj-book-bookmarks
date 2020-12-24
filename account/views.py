d588dfd8e152c8ce0f63a3ca0724f27e2e2ae155
zed-chi <zed-chi@mail.ru>
2020-12-24 12:38:53 +0300

    login part added

--- account/views.py
+++ account/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
-# Create your views here.
+
+def user_login(req):
+    if req.method == "POST":
+        form = LoginForm(req.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(req, username=data["username"], password=data["password"])
+        if user is not None:
+            if user.is_active():
+                login(req,user)
+                return HttpResponse("Auth success")
+            else:
+                return HttpResponse("Disabled account")
+        else:
+            return HttpResponse("Invalid login")
+    else:
+        form = LoginForm()
+    return render(req, "account/login.html", {"form":form})
+
+
+
